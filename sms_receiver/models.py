01338de5bf24e7d6d7b409f40033f0dbf6b276ec
moawezz <moawez.de@gmail.com>
2024-09-05 07:00:21 +0330

    add SMS model

--- sms_receiver/models.py
+++ sms_receiver/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SMS(models.Model):
+    national_code = models.CharField(max_length=10, primary_key=True)
+    phone_number = models.CharField(max_length=15)
+    message = models.TextField()
+    sms_code = models.CharField(max_length=6, null=True, blank=True)
+    received_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"SMS from {self.phone_number} (National Code: {self.national_code})"
